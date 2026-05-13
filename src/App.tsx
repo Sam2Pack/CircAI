ab69c5978733b9b41b56f0717210630bb1b3eaba
Sam2Pack <chakrabortybornil@gmail.com>
2026-05-13 17:29:51 +0530

    commit 14

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home";
 import Assessment from "./pages/Assessment";
 import Results from "./pages/Results";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Assessment />} />
+      <Route path="/" element={<Home />} />
       <Route path="/assessment" element={<Assessment />} />
       <Route path="/results" element={<Results />} />
     </Routes>
@@ -14,4 +15,3 @@ function App() {
 }
 
 export default App;
-
